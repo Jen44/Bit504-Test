63043a83d9f18943a92596316323b310d3b583ca
Jen44 <jenny.olb@hotmail.com>
2022-01-15 17:06:54 +1300

    Testing merging

--- Test10/src/Main.java
+++ Test10/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// Test2
+
 		Scanner scanner = new Scanner(System.in);
 		int age, height;
 		System.out.println("Enter your age: ");
